0d80d2b3a9efa8ffca84e6a662231b74eadeae2e
Keerthika Chokkalingam <lingamkeerthika@gmail.com>
2023-12-09 16:14:01 +0530

    commit with back button tint change based on image color

--- DeskNews/Utils/CustomImageView.swift
+++ DeskNews/Utils/CustomImageView.swift
@@ -13,33 +13,73 @@ class CustomImageView: UIImageView {
     var task: URLSessionDataTask!
     var imageCache = NSCache<AnyObject, AnyObject>()
     
-    func loadImage(urlString: String) {
+//    func loadImage(urlString: String, completion: ((String, Int) -> Void)?=nil) {
+//        self.image = nil
+//
+//        if let task = task {
+//            task.cancel()
+//        }
+//        let url = URL(string: urlString)
+//        guard let url else { return }
+//
+//        if let imageFromCache = imageCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
+//            self.image = imageFromCache
+//            return
+//        }
+//
+//        task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+//            guard let self = self, let data = data, let newImage = UIImage(data: data) else {
+////                print("Cannot load image from url: \(url)")
+//                completion!("failure", 1)
+//                return
+//            }
+//
+//            imageCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
+//
+//            DispatchQueue.main.async { [weak self] in
+//                guard let self else { return }
+//                self.image = newImage
+//                completion!("success", 0)
+//            }
+//        }
+//        task.resume()
+//    }
+    func loadImage(urlString: String, completion: ((String, Int) -> Void)? = nil) {
         self.image = nil
         
         if let task = task {
             task.cancel()
         }
-        let url = URL(string: urlString)
-        guard let url else { return }
+        guard let url = URL(string: urlString) else {
+            completion?("failure", 1)
+            return
+        }
         
         if let imageFromCache = imageCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
             self.image = imageFromCache
+            completion?("success", 0)
             return
         }
         
         task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            guard let self = self, let data = data, let newImage = UIImage(data: data) else {
-//                print("Cannot load image from url: \(url)")
+            guard let self = self, let data = data, error == nil else {
+                completion?("failure", 1)
                 return
             }
             
-            imageCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
-            
-            DispatchQueue.main.async { [weak self] in
-                guard let self else { return }
-                self.image = newImage
+            if let newImage = UIImage(data: data) {
+                imageCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
+                
+                DispatchQueue.main.async {
+                    self.image = newImage
+                    completion?("success", 0)
+                }
+            } else {
+                completion?("failure", 1)
             }
         }
-        task.resume()
+        task?.resume()
     }
+
+    
 }
