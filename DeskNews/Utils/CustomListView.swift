8d7ba449372f3ff230e285a7d8c29ef4fd9e993b
Keerthika <142724355+KeerthikaChokkalingam@users.noreply.github.com>
2024-02-25 15:34:33 +0530

    comit with UI modifications

--- DeskNews/Utils/CustomListView.swift
+++ DeskNews/Utils/CustomListView.swift
@@ -16,18 +16,21 @@ class CustomListView: UIView, UITableViewDelegate, UITableViewDataSource {
         super.init(frame: frame)
         setupListView()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupListView()
     }
-
+    
     private func setupListView() {
         listView = UITableView(frame: self.bounds)
         if let listView = listView {
             listView.delegate = self
             listView.dataSource = self
-            listView.backgroundColor = UIColor().hexStringToUIColor(hex: "#003b4a")
+            listView.backgroundColor = colorManager().mainBgColor
+            listView.separatorStyle = .singleLine
+            listView.layer.borderWidth = 0.5
+            listView.layer.borderColor = colorManager().tabBarTintColor.cgColor
             listView.isScrollEnabled = true
             if #available(iOS 11.0, *) {
                 listView.clipsToBounds = true
@@ -43,15 +46,15 @@ class CustomListView: UIView, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
-            var cellContext = cell.defaultContentConfiguration()
-            cell.backgroundColor = UIColor().hexStringToUIColor(hex: "#003b4a")
-            cellContext.text = listValue[indexPath.row]
-            cellContext.textProperties.color = UIColor.white
+        var cellContext = cell.defaultContentConfiguration()
+        cell.backgroundColor = colorManager().mainBgColor
+        cellContext.text = listValue[indexPath.row]
+        cellContext.textProperties.color = colorManager().tabBarTintColor
         cellContext.textProperties.alignment = .natural
         cellContext.textProperties.numberOfLines = 1
         cellContext.textProperties.font = UIFont(name: "Helvetica Neue", size: 14)!
-            cell.contentConfiguration = cellContext
-            return cell
+        cell.contentConfiguration = cellContext
+        return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 40
